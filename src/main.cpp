2df10afad983533788d5e8984ab9aa12990a7f2e
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2017-06-16 21:44:12 +0200

    Opts parse

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,32 @@
-int main() {
+#include <iostream>
+#include <string>
+
+int die(const std::string& message) {
+  std::cout << message << std::endl;
+  return -1;
+}
+
+int play(const std::string& filename) {
+  std::cerr << "Playing " << filename << std::endl;
   return 0;
+}
+
+int rec(const std::string& filename) {
+  std::cerr << "Recording " << filename << std::endl;
+  return 0;
+}
+
+int main(const int argc, const char** argv) {
+  if (argc < 2) {
+    return die("Missing argument");
+  }
+
+  std::string mode{argv[1]};
+  std::string filename{argv[2]};
+
+  if (mode == "rec") return rec(filename);
+
+  if (mode == "play") return rec(filename);
+
+  return die("Invalid command");
 }
